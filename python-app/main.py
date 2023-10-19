8355e46187d361a35a2da8520af326a3735df6cf
Edil <usobaiev1@gmail.com>
2023-10-19 10:44:13 +0600

    creating a different version of the calender image

--- python-app/main.py
+++ python-app/main.py
@@ -1,6 +1,6 @@
 import calendar
 
-print('Welcome to the Calendar application!')
+print('Welcome to the 2.00 Calendar application!')
 
 year = int(input('Please enter any year:'))
 month = int(input('Please enter any month number:'))
